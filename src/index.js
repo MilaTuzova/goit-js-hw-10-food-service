b28c8f6bff953534cab151a480241b49c4e13b02
MilaTuzova <tuzovals@gmail.com>
2021-06-27 03:27:14 +0300

    added  gallery

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-import './sass/main.scss';
-import './js/main';
+import './templates/menu-gallery.hbs';
+import test from './templates/test-gallery.hbs';
+import menu from './menu.json';
+import './styles.css';
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+
+const ulContainer = document.querySelector('ul.js-menu');
+
+const foodMenuMarkup = createdCardFood(menu)
+
+ulContainer.insertAdjacentHTML('beforeend', foodMenuMarkup);
+
+
+console.table(menu);
+console.log(menu[0].description);
+console.log(menu[0].ingredients);
+console.log(menu[0].ingredients[2])
+
+
+function createdCardFood(card) {
+
+    return card.map(test).join('')
+}
